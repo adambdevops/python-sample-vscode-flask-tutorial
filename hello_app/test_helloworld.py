ab46583a74cacc7625c250acbb69860ec6b86bde
Adam B <adambdevops@outlook.com>
2020-01-20 14:42:10 +0000

    adding main()

--- hello_app/test_helloworld.py
+++ hello_app/test_helloworld.py
@@ -5,3 +5,6 @@ class TestHelloWorldMethods(unittest.TestCase):
 
     def test_helloworld(self):
         self.assertEqual(helloWorld.hello_world(self, "adam"), 'Hello adam')
+
+if __name__ == "__main__":
+    unittest.main()
